f98040310ea32ed3c9dffe49152528b4634f6c88
kchow3 <kchow3@ualberta.ca>
2016-01-12 22:13:53 -0700

    Refactored the code to be more modular

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-#  coding: utf-8 
+#  coding: utf-8
 import SocketServer
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,32 +29,45 @@ import SocketServer
 from os import curdir
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
+    HTTP_OK = "HTTP/1.1 200 OK \r\n"
+    HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found \r\n"
+    HEADER_CONTENT_TYPE = "Content-Type: "
+    HEADER_CONTENT_LENGTH = "Content-Length: "
+    HEADER_CLOSE = "Connection: close \r\n"
+    CRLF = "\r\n"
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        self.length = len(self.data)
-        print ("Got a request of: %s\n" % self.data)
+        self.httpGET()
+        self.sendall(self.response)
+
+    def httpGET(self):
         self.parseRequest()
-        if(self.path.endswith('/')):
-            response = open(curdir + '/www' + self.path + 'index.html', 'r')
-            self.request.sendall('HTTP/1.1 200 OK\r\n Content-Type: text/html\r\n Content-Length: ' + str(72 + self.length + len(response.read())) + '\r\n\r\n' + response.read())
-        else:
-            try:
-                if(self.path.endswith('.html')):
-                    response = open(curdir + '/www' + self.path, 'r')
-                    self.request.sendall('HTTP/1.1 200 OK\r\n Content-Type: text/html\r\n Content-Length: ' + str(72 + self.length + len(response.read())) + '\r\n\r\n' + response.read())
-                elif(self.path.endswith('.css')):
-                    response = open(curdir + '/www' + self.path, 'r')
-                    self.request.send('HTTP/1.1 200 OK\r\n Content-Type: text/css\r\n Content-Length: ' + str(72 + self.length + len(response.read())) + '\r\n\r\n' + response.read())
-                else:
-                    self.request.sendall('HTTP/1.1 404 Not Found\r\n')
-            except:
+        try:
+            if(self.path.endswith('/')):
+                self.file = open(curdir + '/www' + self.path + 'index.html', 'r').read()
+                self.type = 'text/html'
+            elif(self.path.endswith('.html')):
+                self.file = open(curdir + '/www' + self.path, 'r').read()
+                self.type = 'text/html'
+            elif(self.path.endswith('.css')):
+                self.file = open(curdir + '/www' + self.path, 'r').read()
+                self.type = 'text/css'
+            else:
                 self.request.sendall('HTTP/1.1 404 Not Found\r\n')
+            self.length = len(self.file)
+            self.buildResponse()
+        except:
+            self.request.sendall('HTTP/1.1 404 Not Found\r\n')
 
     def parseRequest(self):
         lines = str.splitlines(self.data)
         line1 = lines[0].split()
         self.path = line1[1]
+
+    def buildResponse(self):
+        self.response += HTTP_OK + HEADER_CONTENT_LENGTH + self.length + CRLF + HEADER_CONTENT_TYPE + self.type + CRLF + HEADER_CLOSE + CRLF + self.file
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
